d5593f8ecaf0121931dc66872fde6885a4d3cff5
JuanDiegoCJ <juandiego140904@gmail.com>
2026-05-16 20:16:55 -0500

    Parte 2

--- contracts/17_ExamenPC1.sol
+++ contracts/17_ExamenPC1.sol
@@ -6,6 +6,8 @@ import "hardhat/console.sol";
 
 contract Cafeteria257022 {
 
+    address public dirContrato;
+
     struct Cafe {
         uint256 id;
         string nombre;
@@ -16,5 +18,16 @@ contract Cafeteria257022 {
 
     constructor() {
         console.log("Ejecutado por: 257022 - Juan Diego Carranza Jacinto");
+        dirContrato = address(this);
+    }
+
+    function agregarElemento(uint256 _id, string memory _nombre, uint256 _precio) public {
+        console.log("Ejecutado por: 257022 - Juan Diego Carranza Jacinto");
+        cafes.push(Cafe(_id, _nombre, _precio));
+    }
+
+    function contarElementos() public view returns (uint256) {
+        console.log("Ejecutado por: 257022 - Juan Diego Carranza Jacinto");
+        return cafes.length;
     }
 }
